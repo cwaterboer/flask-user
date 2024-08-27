181ef7fb31712f36c23d18bf5e638ea3dff5f388
Curtis-Lee Waterboer <waterboercurtislee@gmail.com>
2024-08-27 22:57:59 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,11 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-# Initialize the app
 app = Flask(__name__)
-
-# Load configuration from config.py
 app.config.from_object(Config)
 
-# Initialize extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Import routes
+# Import routes after app has been created
 from app import routes
-
-# If you have models, import them as well
-from app import models
